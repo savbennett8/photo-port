f0190c7115c668f593e3305a8325d199f3d55c3b
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-12 19:24:59 -0600

    setup useState for contactForm

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
-    //jsx
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
     return (
         <section>
             <h1>Contact me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea defaultValue={message} name="message" rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
