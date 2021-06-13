6b8a0f823338f08ee01031c78688d73bb24d0a79
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-12 19:31:42 -0600

    synced the state

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,21 +4,26 @@ function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
+    //sync internal state of the 'formState' w/user input from the DOM
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
     return (
         <section>
             <h1>Contact me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea defaultValue={message} name="message" rows="5" />
+                    <textarea defaultValue={message} onChange={handleChange} name="message" rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
